3228fb0a6b7a8c394205e479bb45c9e52db1fbeb
wackyideas <wackyideas535@gmail.com>
2025-04-14 13:31:06 +0200

    Disable authentication notification from showing in history by default

--- misc/uac-polkitagent/install_ninja.sh
+++ misc/uac-polkitagent/install_ninja.sh
@@ -32,6 +32,7 @@ cmake --build .
 echo "Installing..."
 systemctl --user stop plasma-polkit-agent
 kwriteconfig6 --file ~/.config/plasmanotifyrc --group Services --group policykit1-kde --key ShowPopups --notify --type bool false # Disable notification popup
+kwriteconfig6 --file ~/.config/plasmanotifyrc --group Services --group policykit1-kde --key ShowInHistory --notify --type bool false # Disable notification in history
 sudo cp ./bin/polkit-kde-authentication-agent-1 $INSTALLDST
 echo "Restarting systemd service..."
 systemctl --user start plasma-polkit-agent
